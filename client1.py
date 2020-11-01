3be9925c093c1da677390065f5776072e392bbbb
mariiavidmuk <72019772+mariiavidmuk@users.noreply.github.com>
2020-11-01 15:19:37 +0200

    Made a asynchrony server

--- client1.py
+++ client1.py
@@ -1,9 +1,9 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('localhost', 123))
-while (True):
+while True:
     try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(('localhost', 8686))
         x = int(input("Enter the first number: "))
         x = bytes(str(x), "utf-8")
         y = int(input("Enter the second number: "))
@@ -12,14 +12,18 @@ while (True):
         if oper != '+' and oper != '-' and oper != '*' and oper != '/':
             print("Wrong input")
             sock.close()
-            break
         oper = bytes(oper, "utf-8")
+        divisioner = ','
+        divisioner = bytes(str(divisioner), "utf-8")
+        division = '.'
+        division = bytes(str(division), "utf-8")
         sock.send(x)
+        sock.send(divisioner)
         sock.send(y)
+        sock.send(division)
         sock.send(oper)
         result = sock.recv(1024).decode()
         print("The answer is:", result)
     except:
         print("Wrong input")
-        sock.close()
         break
--- server1.py
+++ server1.py
@@ -1,41 +1,114 @@
+import select
 import socket
-import threading
-sock = socket.socket()
-sock.bind(('', 123))
-sock.listen(1)
-global conn
-def recieve():
-    array = []
-    while True:
-        global conn
-        data = conn.recv(1024)
-        data = data.decode("utf-8")
-        array.append(data)
-        x = 0
-        if len(array) == 3:
-            if array[2] == '+':
-                x = int(array[0]) + int(array[1])
-            if array[2] == '-':
-                x = int(array[0]) - int(array[1])
-            if array[2] == '*':
-                x = int(array[0]) * int(array[1])
-            if array[2] == '/':
-                x = int(array[0]) / int(array[1])
-            print(x)
-            conn.send(str(x).encode())
-            array = []
-        if not data:
-            break
-    conn.close()
+
+global dat
+SERVER_ADDRESS = ('localhost', 8686)
+global array
+array = []
+global res
+res = 0
+# Говорит о том, сколько дескрипторов единовременно могут быть открыты
+MAX_CONNECTIONS = 10
+
+# Откуда и куда записывать информацию
+INPUTS = list()
+OUTPUTS = list()
 
 
-def accept_clients():
-    while True:
-        global sock
-        global conn
-        (conn, address) = sock.accept()
-        rT = threading.Thread(target=recieve)
-        rT.start()
-        rT.join()
+def get_non_blocking_server_socket():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setblocking(0)
+    server.bind(SERVER_ADDRESS)
+    server.listen(MAX_CONNECTIONS)
 
-accept_clients()
+    return server
+
+
+def handle_readables(readables, server):
+    for resource in readables:
+        if resource is server:
+            connection, client_address = resource.accept()
+            connection.setblocking(0)
+            INPUTS.append(connection)
+            print("new connection from {address}".format(address=client_address))
+        else:
+            data = ""
+            try:
+                global array
+                data = resource.recv(1024)
+                data = data.decode("utf-8")
+                array.append(data)
+                print(array)
+                a = ""
+                for i in range(len(array)):
+                    a += array[i]
+                print(a)
+                if len(a) == 5:
+                    x = ""
+                    y = ""
+                    oper = a[a.index('.') + 1]
+                    global res
+                    for k in range(a.index(',')):
+                        x += a[k]
+                    x = int(x)
+                    for m in range(a.index(',') + 1, a.index('.')):
+                        y += a[m]
+                    y = int(y)
+                    if oper == "+":
+                        res = x + y
+                    if oper == "-":
+                        res = x - y
+                    if oper == "*":
+                        res = x * y
+                    if oper == "/":
+                        res = x / y
+
+                    array = []
+            except ConnectionResetError:
+                pass
+
+            if data:
+
+                if resource not in OUTPUTS:
+                    OUTPUTS.append(resource)
+
+
+            else:
+
+                clear_resource(resource)
+
+
+def clear_resource(resource):
+    if resource in OUTPUTS:
+        OUTPUTS.remove(resource)
+    if resource in INPUTS:
+        INPUTS.remove(resource)
+    resource.close()
+
+    print('closing connection ' + str(resource))
+
+
+def handle_writables(writables):
+    for resource in writables:
+        try:
+            global res
+            resource.send(str(res).encode())
+
+            res= 0
+        except OSError:
+            clear_resource(resource)
+
+
+if __name__ == '__main__':
+    server_socket = get_non_blocking_server_socket()
+    INPUTS.append(server_socket)
+
+    print("server is running, please, press ctrl+c to stop")
+    try:
+        while INPUTS:
+            readables, writables, exceptional = select.select(INPUTS, OUTPUTS, INPUTS)
+            handle_readables(readables, server_socket)
+            handle_writables(writables)
+    except KeyboardInterrupt:
+        clear_resource(server_socket)
+        print("Server stopped! Thank you for using!")
